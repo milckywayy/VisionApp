de7bf4559b5eec4e093205763a693acf1fac89e8
milckywayy <01178835@pw.edu.pl>
2025-04-11 11:58:22 +0200

    Add text to speech manager

---
+++ app/src/main/java/com/example/visionapp/utils/Speech.kt
@@ -0,0 +1,52 @@
+package com.example.visionapp.utils
+
+import android.content.Context
+import android.speech.tts.TextToSpeech
+import com.example.visionapp.LocalizationConfig
+import java.util.*
+
+
+class TextToSpeechManager(
+    private val context: Context,
+    private val language: Locale = LocalizationConfig.DEFAULT_TTS_LANGUAGE,
+    private val onReady: (() -> Unit)? = null
+) : TextToSpeech.OnInitListener {
+
+    private var tts: TextToSpeech? = null
+    private var isReady = false
+
+    init {
+        tts = TextToSpeech(context, this)
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts?.setLanguage(language)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                throw IllegalStateException("Chosen language is not supported")
+            } else {
+                isReady = true
+                onReady?.invoke()
+            }
+        } else {
+            throw IllegalStateException("Failed to initialize TTS")
+        }
+    }
+
+    fun speak(text: String) {
+        if (isReady) {
+            tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
+        }
+    }
+
+    fun stop() {
+        /**
+         * Immediately stops any ongoing speech output.
+         */
+        tts?.stop()
+    }
+
+    fun shutdown() {
+        tts?.shutdown()
+    }
+}
