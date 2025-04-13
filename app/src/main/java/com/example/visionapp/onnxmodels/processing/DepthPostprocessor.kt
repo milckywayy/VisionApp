74b0ada2e8a2582160e23988a97112c2bbbd4864
Tomasz Jastrzebski <01178848@pw.edu.pl>
2025-04-13 18:35:01 +0200

    Minor fixes in DepthPostprocessor based on KacperChabros suggestions

--- app/src/main/java/com/example/visionapp/onnxmodels/processing/DepthPostprocessor.kt
+++ app/src/main/java/com/example/visionapp/onnxmodels/processing/DepthPostprocessor.kt
@@ -23,14 +23,14 @@ class DepthPostprocessor : IPostprocessor<FloatArray, Bitmap?> {
         val width = array[0].size
         val height = array.size
         val depthBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        for (y in 0 until width) {
-            for (x in 0 until height) {
-                val depthValue = (array[y][x] * 255f).toInt().coerceIn(0, 255)
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                val depthValue = (array[x][y] * 255f).toInt().coerceIn(0, 255)
                 val gray = Color.rgb(depthValue, depthValue, depthValue)
                 depthBitmap.setPixel(x, y, gray)
             }
         }
 
-        return Bitmap.createScaledBitmap(depthBitmap, depthBitmap.width, depthBitmap.height, true)
+        return depthBitmap
     }
 }
