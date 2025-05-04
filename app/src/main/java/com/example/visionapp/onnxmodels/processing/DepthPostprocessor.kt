8386a0d96dca752ac32d6f9e6c8f4bd0ab51939f
KacperChabros <81324685+KacperChabros@users.noreply.github.com>
2025-05-04 15:17:30 +0200

    Optimize Segmentation and Depth Postprocessing (#7)
    * optimize segmentation postprocessing
    
    * optimize depth postprocessing

--- app/src/main/java/com/example/visionapp/onnxmodels/processing/DepthPostprocessor.kt
+++ app/src/main/java/com/example/visionapp/onnxmodels/processing/DepthPostprocessor.kt
@@ -2,8 +2,6 @@ package com.example.visionapp.onnxmodels.processing
 
 import android.graphics.Bitmap
 import android.graphics.Color
-import androidx.core.graphics.createBitmap
-import androidx.core.graphics.set
 
 
 class DepthPostprocessor : IPostprocessor<FloatArray, Bitmap?> {
@@ -23,14 +21,17 @@ class DepthPostprocessor : IPostprocessor<FloatArray, Bitmap?> {
         val width = array[0].size
         val height = array.size
         val depthBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        for (x in 0 until width) {
-            for (y in 0 until height) {
+        val resultPixels = IntArray(width * height)
+
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                val index = y * width + x
                 val depthValue = (array[y][x] * 255f).toInt().coerceIn(0, 255)
                 val gray = Color.rgb(depthValue, depthValue, depthValue)
-                depthBitmap.setPixel(x, y, gray)
+                resultPixels[index] = gray
             }
         }
-
+        depthBitmap.setPixels(resultPixels, 0, width, 0, 0, width, height)
         return depthBitmap
     }
 }
