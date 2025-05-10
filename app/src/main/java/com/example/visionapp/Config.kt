cca1692566f0925935eb9bba6598b9161a94f937
Tomasz Jastrzebski <01178848@pw.edu.pl>
2025-05-10 17:52:06 +0200

    Config fixed

--- app/src/main/java/com/example/visionapp/Config.kt
+++ app/src/main/java/com/example/visionapp/Config.kt
@@ -14,14 +14,13 @@ object CameraConfig {
     val DETECTION_RESOLUTION = Size(640, 640)
     val DEPTH_RESOLUTION = Size(280 , 280)
     const val MAX_BUFFER_SIZE = 5
-    const val CAPTURE_DELAY_MS = 1500L
 }
 
 object ModelsConfig {
     const val SEG_MODEL_PATH = "pp_liteseg.onnx"
     const val DET_MODEL_PATH = "detection.onnx"
     const val DEPTH_MODEL_PATH = "vits_280.onnx"
-    const val DET_MODEL_NUM_OF_CLASSES = 7
+    const val DET_MODEL_NUM_OF_CLASSES = 13
     const val DET_MODEL_CONFIDENCE_THRESHOLD = 0.25f
     const val DET_MODEL_IOU_THRESHOLD = 0.5f
     const val VISUAL_DEBUG_MODE = true
@@ -81,7 +80,6 @@ object CommunicateConfig{
     const val CLEANUP_CHECK_INTERVAL_MS = 2_000L
     const val COMMUNICATE_MAX_AGE_MS = 10_000L
 }
-
 
 object TriangleConfig {
     const val LINE_1_a = -4.139394
