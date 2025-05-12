0de4fe19eae52fd2c0e480f09b1b8e992e96dbb5
Patryk Oleś <patrykoles76@wp.pl>
2025-05-13 00:05:31 +0200

    Add communicate verification by checking 2 frames

--- app/src/main/java/com/example/visionapp/communiates/CommunicateGenerator.kt
+++ app/src/main/java/com/example/visionapp/communiates/CommunicateGenerator.kt
@@ -5,10 +5,29 @@ import com.example.visionapp.onnxmodels.processing.DetectionResult
 
 object CommunicateGenerator {
     fun generateCommunicatesFromDetection(detections: List<DetectionResult>) {
+        val foundTypes = mutableListOf<CommunicateType>()
         for (detection in detections) {
             val type = getDetectionCommunicateType(detection.classId)
             if (type != null) {
-                CommunicateQueue.add(Communicate(type))
+                foundTypes.add(type)
+                if(CommunicateQueue.previousCommunicateStateMap[type] == true){
+                    CommunicateQueue.add(Communicate(type))
+                    CommunicateQueue.previousCommunicateStateMap[type] = false;
+                } else {
+                    CommunicateQueue.previousCommunicateStateMap[type] = true;
+                }
+
+            }
+        }
+        val categoriesToReset = setOf(
+            CommunicateCategory.LIGHTS,
+            CommunicateCategory.SIGNS,
+            CommunicateCategory.CROSSING
+        )
+
+        for (type in CommunicateType.entries) {
+            if (type.category in categoriesToReset && type !in foundTypes) {
+                CommunicateQueue.previousCommunicateStateMap[type] = false
             }
         }
     }
@@ -16,7 +35,17 @@ object CommunicateGenerator {
     fun generateCommunicatesFromTriangle(communicateClass: Int) {
         val type = getTriangleCommunicateType(communicateClass)
         if (type != null) {
-            CommunicateQueue.add(Communicate(type))
+            if(CommunicateQueue.previousCommunicateStateMap[type] == true){
+                CommunicateQueue.add(Communicate(type))
+                CommunicateQueue.previousCommunicateStateMap[type] = false;
+            } else {
+                CommunicateQueue.previousCommunicateStateMap[type] = true;
+            }
+        }
+        for (forType in CommunicateType.entries) {
+            if (forType.category == CommunicateCategory.PASSAGE && forType != type ) {
+                CommunicateQueue.previousCommunicateStateMap[forType] = false
+            }
         }
     }
 
