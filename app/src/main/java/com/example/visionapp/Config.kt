9baf09fe60994a88bf2c64c0aa089b4cec6aaec7
Patryk Oleś <patrykoles76@wp.pl>
2025-06-03 16:21:32 +0200

    change model to YOLOv10 with old classes

--- app/src/main/java/com/example/visionapp/Config.kt
+++ app/src/main/java/com/example/visionapp/Config.kt
@@ -20,10 +20,10 @@ object ModelsConfig {
     const val SEG_MODEL_PATH = "pp_liteseg.onnx"
     const val DET_MODEL_PATH = "detection.onnx"
     const val DEPTH_MODEL_PATH = "vits_280.onnx"
-    const val DET_MODEL_NUM_OF_CLASSES = 13
+    const val DET_MODEL_NUM_OF_CLASSES = 9
     const val DET_MODEL_CONFIDENCE_THRESHOLD = 0.25f
     const val DET_MODEL_IOU_THRESHOLD = 0.5f
-    const val VISUAL_DEBUG_MODE = false
+    const val VISUAL_DEBUG_MODE = true
     const val VISUAL_DEBUG_ONLY_MODE = false
     const val SAVE_TO_FILES = false
 }
